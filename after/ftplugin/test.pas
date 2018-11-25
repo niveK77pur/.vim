53033631a6135b742aecabb7d5d9d7c725c9fcb9
Kevin Laurent Biewesch <KevinBiewesch@yahoo.fr>
2018-11-25 19:00:36 +0100

    Rename .vimrc to vimrc in the .vim folder.
    That way there is no need to create a sym link to it in the
    home directory.

---
+++ after/ftplugin/test.pas
@@ -0,0 +1,17 @@
+{ written by Kevin L. Biewesch     }
+{ Lycée Hubert-Clément classe 1B   }
+{ Date: DD.MM.YYYY                 }
+
+program ExZ_ZpZZ;
+
+{$APPTYPE CONSOLE}
+{$mode objfpc}
+
+uses
+  SysUtils;
+
+  procedure help();
+begin
+	//Enter code
+end.
+
